2b4f2775dfd5f635514cfbb2215e6a05156547c9
Arlo Belshee <arlo@belshee.com>
2024-07-16 14:37:55 -0400

    ^ f Simpler CSS. Removing some presentation.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -46,11 +46,6 @@ header {
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
   width: 100%;
   font-size: 12px;
@@ -65,7 +60,6 @@ nav a.router-link-exact-active {
 nav a.router-link-exact-active:hover {
   background-color: transparent;
 }
-
 nav a {
   display: inline-block;
   padding: 0 1rem;
@@ -75,22 +69,11 @@ nav a {
 nav a:first-of-type {
   border: 0;
 }
-
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
   }
 
   nav {
