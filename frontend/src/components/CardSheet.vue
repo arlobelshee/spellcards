78861ac045e56bbc3bfe176acd7ef02ad39f2fe9
Arlo Belshee <arlo@belshee.com>
2024-08-01 05:55:56 -0400

    . r extract component

---
+++ frontend/src/components/CardSheet.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import { defineProps } from "vue";
+import all_spells from "@/assets/all_spells.json"
+
+const props = defineProps<{ category: string }>();
+const file_path = `@/assets/${props.category}/${useRoute().params.slug}.json`;
+const sheet_data = 'await import(file_path)';
+</script>
+
+<template>
+  <h1>Put lots of cards here!</h1>
+  <p>Loaded from {{ file_path }}:</p>
+  <div>{{ JSON.stringify(sheet_data) }}</div>
+  <p>All spells:</p>
+  <div>{{ JSON.stringify(all_spells) }}</div>
+</template>
--- frontend/src/views/CardSheetView.vue
+++ frontend/src/views/CardSheetView.vue
@@ -1,19 +1,14 @@
 <script setup lang="ts">
-import { useRoute } from "vue-router";
-import { defineProps } from "vue";
-import all_spells from "@/assets/all_spells.json"
+import CardSheet from "@/components/CardSheet.vue";
 
-const props = defineProps<{ category: string }>();
-const file_path = `@/assets/${props.category}/${useRoute().params.slug}.json`;
-const sheet_data = 'await import(file_path)';
+defineProps<{ category: string }>();
 </script>
 
 <template>
   <main>
-    <h1>Put lots of cards here!</h1>
-    <p>Loaded from {{ file_path }}:</p>
-    <div>{{ JSON.stringify(sheet_data) }}</div>
-    <p>All spells:</p>
-    <div>{{ JSON.stringify(all_spells) }}</div>
+    <Suspense>
+      <CardSheet :category="category"></CardSheet>
+      <template #fallback></template>
+    </Suspense>
   </main>
 </template>
