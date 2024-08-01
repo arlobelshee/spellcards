087da40ee5f691c5cd0f387a463c4e9ea33b6822
Arlo Belshee <arlo@belshee.com>
2024-08-01 05:59:49 -0400

    . r inline const & simplify.

--- frontend/src/components/CardSheet.vue
+++ frontend/src/components/CardSheet.vue
@@ -4,13 +4,12 @@ import { defineProps } from "vue";
 import all_spells from "@/assets/all_spells.json"
 
 const props = defineProps<{ category: string }>();
-const file_path = `../assets/${props.category}/${useRoute().params.slug}.json`;
 const sheet_data = await import(`../assets/${props.category}/${useRoute().params.slug}.json`);
 </script>
 
 <template>
   <h1>Put lots of cards here!</h1>
-  <p>Loaded from {{ file_path }}:</p>
+  <p>Stuff to show:</p>
   <div>{{ JSON.stringify(sheet_data) }}</div>
   <p>All spells:</p>
   <div>{{ JSON.stringify(all_spells) }}</div>
