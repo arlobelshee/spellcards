d9c6b50ff1d13b3d8e4554eff2ef812617f60a40
Arlo Belshee <arlo@belshee.com>
2024-08-03 06:41:37 -0400

    ^ F Main app grid layout.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,45 @@ import MainNav from './components/MainNav.vue'
 </script>
 
 <template>
-  <MainNav />
-  <RouterView />
+  <MainNav class="header" />
+  <RouterView class="main" />
 </template>
+<style>
+#app {
+  display: grid;
+  margin: 0;
+  padding: 0;
+  grid: "header header" 70px "main sidebar" auto / auto 20rem;
+  min-height: 100vh;
+}
+
+.header {
+  grid-area: header;
+}
+
+.main {
+  grid-area: main;
+}
+
+.sidebar {
+  grid-area: sidebar;
+}
+
+@media print {
+  #app {
+    grid: "main" auto / auto;
+  }
+
+  #__vue-devtools-container__ * {
+    display: none !important;
+  }
+
+  .header {
+    display: none;
+  }
+
+  .sidebar {
+    display: none;
+  }
+}
+</style>
--- frontend/src/components/MainNav.vue
+++ frontend/src/components/MainNav.vue
@@ -19,98 +19,26 @@ const menu_items = [
 </script>
 
 <template>
-    <Menubar :model="menu_items">
-        <template #item="{ item, props, hasSubmenu, root }">
-            <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
-                <a :href="href" v-bind="props.action" @click="navigate">
-                    <span :class="item.icon"></span>
-                    <span class="ml-2">{{ item.label }}</span>
-                    <i v-if="hasSubmenu"
-                        :class="{ 'pi pi-angle-down ml-2': root, 'pi pi-angle-right ml-auto': !root }"></i>
-                </a>
-            </router-link>
-            <a v-else :href="item.url" :target="item.target" v-bind="props.action">
-                <span :class="item.icon"></span>
-                <span class=" ml-2">{{ item.label }}</span>
-                <i v-if="hasSubmenu" :class="{ 'pi pi-angle-down ml-2': root, 'pi pi-angle-right ml-auto': !root }"></i>
-            </a>
-        </template>
-    </Menubar>
     <header>
         <nav>
-            <ul>
-                <li>
-                    <RouterLink to="/">All</RouterLink>
-                </li>
-                <li>
-                    By Source
-                    <ul>
-                        <li>
-                            <RouterLink to="/by-source/wotc-official-5e">WotC Published 5e</RouterLink>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    By Class
-                    <ul>
-                        <li>
-                            <RouterLink to="/by-class/wizard">Wizard</RouterLink>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    Characters
-                    <ul>
-                        <li>
-                            <RouterLink to="/characters/fedele">Fedele Pons Civitas</RouterLink>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
+            <Menubar :model="menu_items">
+                <template #item="{ item, props, hasSubmenu, root }">
+                    <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+                        <a :href="href" v-bind="props.action" @click="navigate">
+                            <span :class="item.icon"></span>
+                            <span class="ml-2">{{ item.label }}</span>
+                            <i v-if="hasSubmenu"
+                                :class="{ 'pi pi-angle-down ml-2': root, 'pi pi-angle-right ml-auto': !root }"></i>
+                        </a>
+                    </router-link>
+                    <a v-else :href="item.url" :target="item.target" v-bind="props.action">
+                        <span :class="item.icon"></span>
+                        <span class=" ml-2">{{ item.label }}</span>
+                        <i v-if="hasSubmenu"
+                            :class="{ 'pi pi-angle-down ml-2': root, 'pi pi-angle-right ml-auto': !root }"></i>
+                    </a>
+                </template>
+            </Menubar>
         </nav>
     </header>
 </template>
-<style scoped>
-header {
-    line-height: 1.5;
-    max-height: 100vh;
-}
-
-nav {
-    width: 100%;
-    font-size: 12px;
-    text-align: center;
-    margin-top: 2rem;
-}
-
-nav a.router-link-exact-active:hover {
-    background-color: transparent;
-}
-
-nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-    border: 0;
-}
-
-@media (min-width: 1024px) {
-    header {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
-
-    nav {
-        text-align: left;
-        margin-left: -1rem;
-        font-size: 1rem;
-
-        padding: 1rem 0;
-        margin-top: 1rem;
-    }
-}
-</style>
