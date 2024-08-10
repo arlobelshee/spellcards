78c489e8e88f6d494c3968c5a9531d8bfbb05b32
Arlo Belshee <arlo@belshee.com>
2024-08-10 07:01:56 -0400

    ^ F Can start to import actual MPMB content! Of course, their format requires being run as a script (eval) in order to import, which is horrible.

--- frontend/scripts/import_mpmb.ts
+++ frontend/scripts/import_mpmb.ts
@@ -2,14 +2,26 @@ import fs from "node:fs/promises";
 import process from "node:process";
 import console from "node:console";
 
+const AtHigherLevels = "MARKER_AT_HIGHER_LEVELS";
+
 async function import_everything(args: string[]) {
 	const script = await fs.readFile(args[0], { encoding: "utf8" });
 	const input = get_interesting_raw_data(script);
-	console.log(input);
+	console.log(input, Object.keys(input.spells).length);
 }
 
 function get_interesting_raw_data(script: string) {
-	return script.slice(0, 1000);
+	const results = { spells: {}, sources: {} };
+	const other = { sheetVersion: 13001014, RequiredSheetVersion() {} };
+	Function(
+		"sheetVersion",
+		"RequiredSheetVersion",
+		"SpellsList",
+		"SourceList",
+		"AtHigherLevels",
+		`"use strict";${script}`,
+	)(other.sheetVersion, other.RequiredSheetVersion, results.spells, results.sources, AtHigherLevels);
+	return results;
 }
 
 await import_everything(process.argv.slice(2));
