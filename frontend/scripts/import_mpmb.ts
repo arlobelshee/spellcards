b7ed35938a120aac410eda488a046b0346963919
Arlo Belshee <arlo@belshee.com>
2024-08-16 16:53:06 -0400

    ^ F Output the path to the all_spells.json file that I'll merge into.

--- frontend/scripts/import_mpmb.ts
+++ frontend/scripts/import_mpmb.ts
@@ -1,17 +1,24 @@
 import fs from "node:fs/promises";
+import path from "node:path";
 import process from "node:process";
 import console from "node:console";
 
 const AtHigherLevels = "MARKER_AT_HIGHER_LEVELS";
 
 async function import_everything(args: string[]) {
+	if (args.length !== 2) {
+		console.error("Expected 2 args: a source file and the destination folder.");
+		return;
+	}
+	const dest_folder = args[1];
 	const script = await fs.readFile(args[0], { encoding: "utf8" });
 	const input = get_interesting_raw_data(script);
-	merge_into_spells_data(input);
+	merge_into_spells_data(input, path.join(dest_folder, "all_spells.json"));
 }
 
-function merge_into_spells_data(input: { spells: {}; sources: {} }) {
+function merge_into_spells_data(input: { spells: {}; sources: {} }, dest_path: string) {
 	console.log(input, Object.keys(input.spells).length);
+	console.log("writing to", dest_path);
 }
 
 function get_interesting_raw_data(script: string) {
