88169b209949c58f2ba05f41e49995f97a44868b
Arlo Belshee <arlo@belshee.com>
2024-08-17 06:27:59 -0400

    ^ F Now actually handles all the description fields in the source. Also adding lots of typing.

--- frontend/scripts/import_mpmb.ts
+++ frontend/scripts/import_mpmb.ts
@@ -4,6 +4,13 @@ import process from "node:process";
 import console from "node:console";
 
 const AtHigherLevels = "MARKER_AT_HIGHER_LEVELS";
+type DestSpell = {
+	id: string;
+	name: string;
+	name_short?: string;
+	description: { short: string; base: string; upcast?: string; cantrip?: string };
+};
+type AllSpellsFileContents = { version: number; kind: "spell-list"; spells: Record<string, DestSpell> };
 
 async function import_everything(args: string[]) {
 	if (args.length !== 2) {
@@ -16,15 +23,19 @@ async function import_everything(args: string[]) {
 	await merge_into_spells_data(input, path.join(dest_folder, "all_spells.json"));
 }
 
-type SourceSpell = { description: String } & Record<string, unknown>;
+type SourceSpell = { description: string; descriptionFull?: string; descriptionCantripDie?: string } & Record<
+	string,
+	unknown
+>;
 const KeysToKeepUnaltered = [
 	["name", "name"],
 	["nameShort", "name_short"],
 ];
+type ImportedSpell = Omit<DestSpell, "id" | "description">;
 
 async function merge_into_spells_data(input: { spells: Record<string, SourceSpell>; sources: {} }, dest_path: string) {
 	console.log(input, Object.keys(input.spells).length);
-	let dest_data = JSON.parse(await fs.readFile(dest_path, { encoding: "utf8" }));
+	let dest_data: AllSpellsFileContents = JSON.parse(await fs.readFile(dest_path, { encoding: "utf8" }));
 	if (!dest_data || dest_data.kind !== "spell-list" || dest_data.version !== 1) {
 		console.log("Invalid data found in", dest_path, "re-initializing it.");
 		dest_data = { version: 1, kind: "spell-list", spells: {} };
@@ -34,13 +45,20 @@ async function merge_into_spells_data(input: { spells: Record<string, SourceSpel
 		if (Object.prototype.hasOwnProperty.call(dest_data.spells, new_spell[0])) {
 			console.log("WARNING: replacing spell with ID", new_spell[0]);
 		}
-		const [desc_base, desc_higher_levels] = new_spell[1].description.split(AtHigherLevels, 2);
+		const [desc_base, desc_higher_levels] = new_spell[1].descriptionFull.split(AtHigherLevels, 2);
 		dest_data.spells[new_spell[0]] = {
-			...Object.fromEntries(KeysToKeepUnaltered.map((key) => [key[1], new_spell[1][key[0]]])),
+			...(Object.fromEntries(KeysToKeepUnaltered.map((key) => [key[1], new_spell[1][key[0]]])) as ImportedSpell),
 			id: new_spell[0],
-			description: { base: desc_base, higher_levels: desc_higher_levels },
+			description: { short: new_spell[1].description, base: desc_base.trim(), upcast: desc_higher_levels.trim() },
 		};
-		console.log(dest_data, Object.keys(dest_data.spells).length);
+		console.log(JSON.stringify(dest_data, undefined, "\t"), Object.keys(dest_data.spells).length);
+		console.log(
+			JSON.stringify(
+				Object.values(dest_data.spells).filter((v) => !!v.description?.upcast),
+				undefined,
+				"\t",
+			),
+		);
 	}
 }
 
