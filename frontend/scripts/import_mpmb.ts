1b165afedd4d2c72969ae47b301fc37d373b5579
Arlo Belshee <arlo@belshee.com>
2024-08-17 06:03:23 -0400

    ^ F MPMB importer now gets spells and is starting to extract keys we care about. Handles name, short name, and description.

--- frontend/scripts/import_mpmb.ts
+++ frontend/scripts/import_mpmb.ts
@@ -13,12 +13,35 @@ async function import_everything(args: string[]) {
 	const dest_folder = args[1];
 	const script = await fs.readFile(args[0], { encoding: "utf8" });
 	const input = get_interesting_raw_data(script);
-	merge_into_spells_data(input, path.join(dest_folder, "all_spells.json"));
+	await merge_into_spells_data(input, path.join(dest_folder, "all_spells.json"));
 }
 
-function merge_into_spells_data(input: { spells: {}; sources: {} }, dest_path: string) {
+type SourceSpell = { description: String } & Record<string, unknown>;
+const KeysToKeepUnaltered = [
+	["name", "name"],
+	["nameShort", "name_short"],
+];
+
+async function merge_into_spells_data(input: { spells: Record<string, SourceSpell>; sources: {} }, dest_path: string) {
 	console.log(input, Object.keys(input.spells).length);
-	console.log("writing to", dest_path);
+	let dest_data = JSON.parse(await fs.readFile(dest_path, { encoding: "utf8" }));
+	if (!dest_data || dest_data.kind !== "spell-list" || dest_data.version !== 1) {
+		console.log("Invalid data found in", dest_path, "re-initializing it.");
+		dest_data = { version: 1, kind: "spell-list", spells: {} };
+	}
+	console.log(dest_data);
+	for (const new_spell of Object.entries(input.spells)) {
+		if (Object.prototype.hasOwnProperty.call(dest_data.spells, new_spell[0])) {
+			console.log("WARNING: replacing spell with ID", new_spell[0]);
+		}
+		const [desc_base, desc_higher_levels] = new_spell[1].description.split(AtHigherLevels, 2);
+		dest_data.spells[new_spell[0]] = {
+			...Object.fromEntries(KeysToKeepUnaltered.map((key) => [key[1], new_spell[1][key[0]]])),
+			id: new_spell[0],
+			description: { base: desc_base, higher_levels: desc_higher_levels },
+		};
+		console.log(dest_data, Object.keys(dest_data.spells).length);
+	}
 }
 
 function get_interesting_raw_data(script: string) {
