74b785e5b14a3ad2f44c5b45fa9f31d5d3f5475c
Arlo Belshee <arlo@belshee.com>
2024-08-19 06:04:44 -0400

    . d More notes on what I'm going to build for character display & how it'll play. 2 Books! Swapping pages out! Oh, the fun.

--- frontend/src/components/CardSheet.vue
+++ frontend/src/components/CardSheet.vue
@@ -21,13 +21,17 @@ watchEffect(
           Right nav:
           <ul>
             <li>
-              Can enter computed char values. Spellcasting classes, each with level & bonus to atk & DC. 6 attribute
-              final adjusted modifiers. System will compute proficinecy bonus, then atk & DC for that class. Will also
-              compute total spell slots (both regular & pact magic).
+              Can enter computed char values. Spellcasting classes, each with level & bonus to atk, DC, and memorized
+              spells. 6 attribute final adjusted modifiers. System will compute proficinecy bonus, then atk, DC, &
+              memorization slots by class. Will also compute total spell slots (both regular & pact magic).
             </li>
             <li>
-              Can select spells for Wizard, Sorc, Warlock. Not for Cleric, Druid. Well, for Cleric & Druid can select
-              bonus spells (domain, etc).
+              Can select spells for Wizard, Sorc, Warlock, Bard, Artificer. Not for Cleric, Druid. Well, for Cleric &
+              Druid can select bonus spells (domain, etc).
+            </li>
+            <li>
+              Can enter limited effects. These are like spells - and may even be added to the system as if they were
+              spells. They will just appear differently on the spell slot & limited effect organizer.
             </li>
           </ul>
         </li>
@@ -58,25 +62,79 @@ watchEffect(
                   memorization, for one common circumstance.
                 </li>
                 <li>
-                  It can print out blank (but named context) spell loadout sheets. I don't know how individual spells
-                  cards are attached to a loadout sheet.
+                  These are tiny, using icons mostly. They also include the character's RP suggestions per spell. This
+                  is the place to edit those. Can also edit in spellbook, if desired.
                 </li>
-                <li>These are tiny, using icons mostly. They also include the character's RP suggestions per spell. This
-                  is the place to edit those. Can also edit in spellbook, if desired.</li>
                 <li>Can easily print several copies of each one. Pick a number; shows that many.</li>
+                <li>
+                  Can assign a character-specific name to these. Will be used on the memorization card, with full
+                  game-rules name listed for reference.
+                </li>
               </ul>
             </li>
-            <li>Spellbook reference cards:<ul>
-                <li>These print the same for all characters, so can re-use the spellbook section & don't have to
-                  re-print cards when leveling. Can select on any card that it has already been printed, and it'll move
-                  to the non-printing cards section.</li>
-                <li>On screen, they include the RP suggestions per spell, though those suggestions only actually appear
-                  in the morized spell cards display when printed.</li>
-                <li>Can pick the number of memorization mini-cards to print for each spell card - whether that spell
-                  card is being printed or not.
+            <li>
+              Spellbook reference cards:
+              <ul>
+                <li>
+                  These print the same for all characters, so can re-use the spellbook section & don't have to re-print
+                  cards when leveling. Can select on any card that it has already been printed, and it'll move to the
+                  non-printing cards section.
                 </li>
-                <li>Have a section to show, greyed out and just names, all the spells that are already printed. Can use
-                  this to select one to print again.</li>
+                <li>
+                  On screen, they include the RP suggestions per spell, though those suggestions only actually appear in
+                  the morized spell cards display when printed.
+                </li>
+                <li>
+                  Can pick the number of memorization mini-cards to print for each spell card - whether that spell card
+                  is being printed or not.
+                </li>
+                <li>
+                  Have a section to show, greyed out and just names, all the spells that are already printed. Can use
+                  this to select one to print again.
+                </li>
+              </ul>
+            </li>
+            <li>
+              Limited effect organizer page:
+              <ul>
+                <li>Intended to store in the character book and be pulled out for play.</li>
+                <li>
+                  Includes Atk/DC by class, in tight form. These are color-coded, with colors matching the memorization
+                  card colors. Also indicates number of memorizations you get.
+                </li>
+                <li>
+                  Prints out a page intended to hold dice to show slots remaining, refresh times, etc. Each item has a
+                  square sized to fit a d6. Inside the square is the number you get on a refresh. Under the square is
+                  the kind of rest that lets you refresh.
+                </li>
+                <li>
+                  Includes regular casting slots by level. Also includes pact magic slots, which indicate their level
+                  (basically like regular slots, but without other regular slots around them, and with different
+                  refresh)
+                </li>
+                <li>Non-slot actions are like slots unless they are special cased.</li>
+                <li>
+                  Arcane abeyance, eg, contains 2 blank spots each the size of a memorization spell. Can put one on to
+                  store a bead, then turn upside down to show slot was used. Spell storing devices could work similarly.
+                </li>
+              </ul>
+            </li>
+            <li>
+              Spell loadout page:
+              <ul>
+                <li>
+                  Intended to store in the character book with one at a time pulled out for play, to match expected
+                  circumstances. Includes a name for the circumstance. I don't know how individual spell cards are
+                  attached to a loadout sheet.
+                </li>
+                <li>
+                  Can be printed with fixed choices (Bard, Sorc, Warlock), or for selection (Cleric, Wizard, Druid).
+                  Special actions are also pre-printed, since they are always available.
+                </li>
+                <li>
+                  They are merged across all classes, so multi-class might have some fixed in slot + some open spaces.
+                </li>
+                <li>Segments spells by level & by action type, for fast use in combat.</li>
               </ul>
             </li>
           </ul>
