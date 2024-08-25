e1e49ee4f3247f8b7e032cf451d6abc7e8c2b705
Arlo Belshee <arlo@belshee.com>
2024-08-25 08:04:24 -0400

    . f Update notes to self & put them in the right controls.

--- frontend/src/components/CardFilterControls.vue
+++ frontend/src/components/CardFilterControls.vue
@@ -3,4 +3,28 @@
 
 <template>
   <h1>All the controls go here!</h1>
+  <p>Notes:</p>
+  <ul>
+    <li>
+      For characters:
+      <ul>
+        <li>
+          Can enter computed char values. Spellcasting classes, each with level & bonus to atk, DC, and memorized
+          spells. 6 attribute final adjusted modifiers. System will compute proficinecy bonus, then atk, DC, &
+          memorization slots by class. Will also compute total spell slots (both regular & pact magic).
+        </li>
+        <li>
+          Can select spells for Wizard, Sorc, Warlock, Bard, Artificer. Not for Cleric, Druid. Well, for Cleric &
+          Druid can select bonus spells (domain, etc).
+        </li>
+        <li>
+          Can enter limited effects. These are like spells - and may even be added to the system as if they were
+          spells. They will just appear differently on the spell slot & limited effect organizer.
+        </li>
+      </ul>
+    </li>
+    <li>For classes: Dunno. Filter by source?</li>
+    <li>For sources: Dunno. Filter by class?</li>
+    <li>For all spells list: Dunno. Filter by level & class? Filter by partial name match?</li>
+  </ul>
 </template>
