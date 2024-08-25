04fec0180c6bd43cf431225dc6a67d540c3bcfce
Arlo Belshee <arlo@belshee.com>
2024-08-25 08:10:49 -0400

    . f Store starts having a set of active_spells, which will eventually be used to filter based on current route & control selections.

--- frontend/src/stores/active_cards_store.ts
+++ frontend/src/stores/active_cards_store.ts
@@ -1,7 +1,14 @@
 import { defineStore } from "pinia";
 import all_spells from "@/assets/all_spells.json";
-
+type SpellSelectionData = { id: string; already_printed: boolean };
 export const useSpellsStore = defineStore("spells.active", {
-	state: () => ({ all_spells }),
+	state: () => ({
+		all_spells,
+		active_spells: {
+			descriptions: [] as SpellSelectionData[],
+			rituals: [] as SpellSelectionData[],
+			selectable: [] as SpellSelectionData[],
+		},
+	}),
 	actions: {},
 });
