ae580be486dcc9467c9c5a03947a197d22cfece1
Arlo Belshee <arlo@belshee.com>
2024-08-25 08:35:09 -0400

    . a whitespace

--- frontend/src/boundaries/engine/index.ts
+++ frontend/src/boundaries/engine/index.ts
@@ -1,6 +1,7 @@
 export * from "./api";
 import type { SpellFilter as SpellFilter_ } from "./api";
 import { EmptySpellFilter } from "./spell_selection";
+
 export const SpellFilter = {
 	create_empty(): SpellFilter_ {
 		return new EmptySpellFilter();
